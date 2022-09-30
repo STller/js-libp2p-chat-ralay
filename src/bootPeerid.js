f6ac3404956663674e4ecd2d9e52c92b52edf466
STller <1165975843@qq.com>
2022-09-30 10:45:45 +0800

    save

--- src/bootPeerid.js
+++ src/bootPeerid.js
@@ -1,5 +1,5 @@
 export default  [
-    '/ip4/10.10.2.224/tcp/55275/p2p/12D3KooWPTLCy2Gqp7nzb3QmhuVv4WGR5SHKzQE1MTEz72MmYaJc',
+    '/ip4/169.254.162.184/tcp/55275/p2p/12D3KooWQYjBiEf6pFFVzb1dqdxVWAC8QSEPE3FgRpewqtQqeYE4',
     // '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
     // '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
     // '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
